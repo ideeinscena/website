2375cf687fffa980ab2eaf0b01bcc95423ef906c
Maurizio De Magnis <root@olisti.co>
2016-08-31 16:00:47 +0200

    Various stuff
    1. structure
    2. navbar
    3. about page

--- config.rb
+++ config.rb
@@ -6,6 +6,14 @@ activate :dato,
   token: "1ed3648af9035435aec1db11c3800726b9b4385e51a31f0c0d",
   base_url: "https://www.ideeinscena.it"
 
+activate :directory_indexes
+
+ignore "/templates/*"
+
+dato.shows.each do |show|
+  proxy "/show/#{show.slug}.html", "/templates/show.html", locals: { show: show }
+end
+
 configure :build do
   activate :minify_css
   activate :minify_javascript
